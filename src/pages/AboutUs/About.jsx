729110a8b9f3152224d74ae00370a8d8cdd57982
HaroldXL <kalkelvinxl@gmail.com>
2026-05-19 12:03:04 +0700

    Update avatar

--- src/pages/AboutUs/About.jsx
+++ src/pages/AboutUs/About.jsx
@@ -1,5 +1,16 @@
 import { Button, Card, CardBody } from "@heroui/react";
-import { AltArrowRight, CupFirst, Eye, Global, HandShake, HeartShine, Lightbulb, Rocket, Target, UsersGroupRounded } from "@solar-icons/react"
+import {
+  AltArrowRight,
+  CupFirst,
+  Eye,
+  Global,
+  HandShake,
+  HeartShine,
+  Lightbulb,
+  Rocket,
+  Target,
+  UsersGroupRounded,
+} from "@solar-icons/react";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
@@ -8,7 +19,6 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { useThemeColors } from "../../hooks/useThemeColors";
 import { useTheme } from "../../contexts/ThemeContext";
-
 
 // eslint-disable-next-line no-unused-vars
 const { motion } = MotionLib;
@@ -32,7 +42,7 @@ const itemVariants = {
 
 // Import images
 import avatarGiap from "../../assets/images/avatar-Giap.jpg";
-import avatarKaius from "../../assets/images/avatar-kaius.png";
+import avatarKaius from "../../assets/images/alovu.jpg";
 import avatarThien from "../../assets/images/avatar-Thien.png";
 import avatarCuong from "../../assets/images/avatar-Cuong.jpg";
 import projectImage from "../../assets/illustrations/selfie.avif";
@@ -320,16 +330,16 @@ const About = () => {
                 bgColor: "rgba(59, 130, 246, 0.1)",
               },
               {
-                icon: (props) => (
-                  <Global weight="BoldDuotone" {...props} />
-                ),
+                icon: (props) => <Global weight="BoldDuotone" {...props} />,
                 title: t("about.story.headquarters"),
                 value: "Ho Chi Minh City, Vietnam",
                 color: "#10B981",
                 bgColor: "rgba(16, 185, 129, 0.1)",
               },
               {
-                icon: (props) => <UsersGroupRounded weight="BoldDuotone" {...props} />,
+                icon: (props) => (
+                  <UsersGroupRounded weight="BoldDuotone" {...props} />
+                ),
                 title: t("about.story.teamSize"),
                 value: "5",
                 color: "#F59E0B",
@@ -550,7 +560,9 @@ const About = () => {
                 color: colors.primary.main,
               }}
               onPress={() => navigate("/register")}
-              endContent={<AltArrowRight weight="BoldDuotone" className="w-5 h-5" />}
+              endContent={
+                <AltArrowRight weight="BoldDuotone" className="w-5 h-5" />
+              }
             >
               {t("about.cta.button")}
             </Button>
